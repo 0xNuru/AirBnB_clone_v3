f29e2e8de507391ec5389ac60343382e17336302
0xNuru <nurudeenmyself@yahoo.com>
2024-03-31 15:04:15 +0100

    add route for stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,17 @@ from models import storage
 def status():
     """Returns a JSON of ok"""
     return jsonify(status='OK')
+
+
+@app_views.route('/stats')
+def stats():
+    """ Retrieves the number of each objects by type """
+    classes = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(classes)
