4bab29286cbc87e3ea10778496fb070e68c2dd2f
pasebukky <olubukunolapase@gmail.com>
2024-03-31 17:32:21 +0100

    Error handling

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,21 +5,18 @@ from flask import jsonify
 from flask import abort
 from flask import request
 from api.v1.views import app_views, storage
-from models.amenity import Amenity
 from models.city import City
 from models.place import Place
-from models.state import State
-from models.user import User
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
 def get_all_places(city_id):
     """ Retrieves the list of all Place objects of a City """
-    list_of_places = []
-    city = storage.get("City", str(city_id))
+    city = storage.get(City, city_id)
     if city is None:
         abort(404)
+    list_of_places = []
     for place in city.places:
         list_of_places.append(place.to_dict())
     return jsonify(list_of_places)
@@ -50,18 +47,20 @@ def delete_place(place_id):
                  strict_slashes=False)
 def create_place(city_id):
     """ Creates a Place """
-    json_place = request.get_json()
-    if json_place is None:
-        abort(400, description='Not a JSON')
-    if not storage.get("User", json_place["user_id"]):
-        abort(404, description='User not found')
-    if not storage.get("City", city_id):
+    city = storage.get(City, city_id)
+    if city is None:
         abort(404)
-    if "user_id" not in json_place:
-        abort(400, description='Missing user_id')
-    if "name" not in json_place:
-        abort(400, description='Missing name')
-    json_place["city_id"] = city_id
+    json_place = request.get_json()
+    if not json_place:
+        abort(400, 'Not a JSON')
+    if 'user_id' not in json_place:
+        abort(400, 'Missing user_id')
+    user_id = json_place['user_id']
+    if storage.get(User, user_id) is None:
+        abort(404)
+    if 'name' not in json_place:
+        abort(400, 'Missing name')
+    json_place['city_id'] = city_id
     new_place = Place(**json_place)
     storage.new(new_place)
     storage.save()
@@ -75,8 +74,8 @@ def update_place(place_id):
     if place is None:
         abort(404)
     json_place = request.get_json()
-    if json_place is None:
-        abort(400, description='Not a JSON')
+    if not json_place:
+        abort(400, 'Not a JSON')
     for key, value in json_place.items():
         if key not in ('id', 'user_id', 'city_id', 'created_at', 'updated_at'):
             setattr(place, key, value)
