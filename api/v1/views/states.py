5a2bab017e23044e6dfa29d504fe6106cf8e363e
pasebukky <olubukunolapase@gmail.com>
2024-04-01 10:33:39 +0100

    Revert

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,7 +6,6 @@ from flask import abort
 from flask import request
 from api.v1.views import app_views, storage
 from models.state import State
-from models import storage
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
